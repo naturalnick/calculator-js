1378457d11d9aae041af6072c664373cde3fd034
Nick Schaefer <114598406+naturalnick@users.noreply.github.com>
2022-10-15 00:26:06 -0400

    progress...

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 
 
-let firstNum = "";
-let SecondNum = "";
+let firstNumber = 0;
+let secondNumber = 0;
 let operand = "";
+let resetDisplay = false;
 
 const buttons = document.getElementsByClassName("btn");
 
 Array.from(buttons).forEach(element => {
     element.addEventListener("click", function (event) {
-        const buttonText = this.innerHTML
+        const buttonText = this.innerHTML;
+
         if (parseInt(buttonText) || buttonText == 0 || buttonText === ".") {
             generateNumber(buttonText);
+            deselectOperand();
         } else {
 
             switch (buttonText) {
                 case "CE":
-                    //clear
+                    
                     break;
                 case "÷":
-                    operand = buttonText;
-                    break;
                 case "x":
-                    operand = buttonText;
-                    break;
                 case "-":
-                    operand = buttonText;
-                    break;
                 case "+":
+                    secondNumber = firstNumber;
+                    firstNumber = 0;
                     operand = buttonText;
+                    element.classList.add("selected");
+                    resetDisplay = true;
                     break;
                 case "=":
-                    //calculate
+                    firstNumber = calculate(firstNumber, operand, secondNumber);
+                    secondNumber = 0;
+                    document.getElementById("display").innerHTML = firstNumber;
                     break;
                 case "M+":
 
@@ -44,9 +47,6 @@ Array.from(buttons).forEach(element => {
                 case "MC":
 
                     break;
-                case ".":
-                    //maybe put decimal with numbers
-                    break;
                 default:
                     break;
             }
@@ -54,11 +54,47 @@ Array.from(buttons).forEach(element => {
     });
 });
 
-function generateNumber(numStr) {
 
+
+function generateNumber(numStr) {
+    if (resetDisplay) document.getElementById("display").innerHTML = "";
+    let numberStr = document.getElementById("display").innerHTML;
+    numberStr += numStr;
+    document.getElementById("display").innerHTML = numberStr;
+    firstNumber = parseFloat(numberStr);
+    console.log(firstNumber);
 }
 
 function calculate(num1, op, num2) {
-    //switch statement for operators
-    //return result
+    let result;
+    switch (op) {
+        case "÷":
+            result = num1 / num2;
+            break;
+        case "x":
+            result = num1 * num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "+":
+            result = num1 + num2;
+            break;
+        default:
+            break;
+    }
+    console.log(result);
+    return result.toFixed(3);
+    //need to not show decimals.000
+}
+
+
+
+function deselectOperand() {
+
+    Array.from(buttons).forEach(element => {
+        if (element.classList.contains("selected")) {
+            element.classList.remove("selected");
+        }
+    })
 }
