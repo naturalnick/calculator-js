fea6c395eb6e1770326e071cce2f2504737b7961
Nick Schaefer <wildernns@gmail.com>
2022-10-24 14:10:13 -0400

    add finish calculaton on equals

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
-let firstNumber = 0;
-let secondNumber = 0;
 let memoryNumber = 0;
 
-let operator = "";
+let operationStarted = false;
 let operatorSelected = false;
-let operationReady = false
+let operationReady = false;
 
 const numButtons = document.getElementsByClassName("num-btn");
 const opButtons = document.getElementsByClassName("op-btn");
@@ -13,161 +10,183 @@ const memButtons = document.getElementsByClassName("mem-btn");
 const equalButton = document.getElementById("eql-btn");
 const clearButton = document.getElementById("clr-btn");
 
+//TODO add exponent functionality
+//TODO save sliced decimals for use in calculation
+
 for (let button of numButtons) {
-    button.addEventListener("click", function (event) {
-        const newNum = concatenateNumber(button.textContent);
-        setDisplayValue(newNum);
-    });
+	button.addEventListener("click", function (event) {
+		const newNum = concatenateNumber(button.textContent);
+		setDisplayValue(newNum);
+	});
 }
 
 for (let button of opButtons) {
-    button.addEventListener("click", selectOperator);
+	button.addEventListener("click", selectOperator);
 }
 
 for (let button of memButtons) {
-    button.addEventListener("click", function () {
-        switch (button.textContent) {
-            case "M+":
-            case "M-":
-                changeMemory(button.textContent);
-                break;
-            case "MR":
-                recallMemory();
-                break;
-            case "MC":
-                clearMemory();
-                break;
-            default:
-                break;
-        }
-    });
+	button.addEventListener("click", function () {
+		switch (button.textContent) {
+			case "M+":
+			case "M-":
+				changeMemory(button.textContent);
+				break;
+			case "MR":
+				recallMemory();
+				break;
+			case "MC":
+				clearMemory();
+				break;
+			default:
+				break;
+		}
+	});
 }
 
 equalButton.addEventListener("click", performOperation);
 
 clearButton.addEventListener("click", clearAll);
 
-function concatenateNumber(num) {
-    if (operatorSelected) {
-        setDisplayValue("");
-        deselectOperators();
-    }
-    let numberStr = getDisplayValue();
-    numberStr += num;
-    if (operator === "") { //operator is only empty while entering the firstNumber, this works, but replace with new boolean later
-        firstNumber = parseFloat(numberStr);
-    } else {
-        secondNumber = parseFloat(numberStr);
-        operationReady = true;
-    }
-    return numberStr;
-}
+const operation = {
+	num1: 0,
+	num2: 0,
+	operator: "",
+	operate() {
+		switch (this.operator) {
+			case "÷":
+				return this.num1 / this.num2;
+			case "x":
+				return this.num1 * this.num2;
+			case "-":
+				return this.num1 - this.num2;
+			case "+":
+				return this.num1 + this.num2;
+			default:
+				break;
+		}
+	},
+	reset() {
+		this.num1 = 0;
+		this.num2 = 0;
+		this.operator = "";
+	},
+};
 
-function performOperation() {
-    if (operationReady) {
-        const newNum = operators[operator].operate(firstNumber, secondNumber);
-        firstNumber = formatResultForDisplay(newNum);
-        setDisplayValue(firstNumber);
-        secondNumber = 0;
-        operationReady = false;
-    }
+function performOperation(event) {
+	if (operationReady) {
+		const newNum = operation.operate();
+		operation.num1 = formatResultForDisplay(newNum);
+		setDisplayValue(operation.num1);
+		if (operatorSelected) {
+			operation.num2 = 0;
+		} else {
+			operation.reset();
+		}
+		operationReady = false;
+		if (event.target.textContent === "=") {
+			operationStarted = false;
+		}
+	}
 }
 
 function selectOperator(event) {
-    if (firstNumber != 0) {
-        if (secondNumber != 0) performOperation();
-        operator = event.target.textContent;
-        deselectOperators();
-        event.target.classList.add("selected");
-        operatorSelected = true;
-    }
+	if (operation.num1 != 0) {
+		deselectOperators();
+		event.target.classList.add("selected");
+		operatorSelected = true;
+		if (operation.num2 != 0) performOperation();
+		operation.operator = event.target.textContent;
+	}
 }
 
-//TODO add exponent functionality
-//TODO save sliced decimals for use in calculation
+function concatenateNumber(num) {
+	if (operatorSelected) {
+		setDisplayValue("0");
+		deselectOperators();
+	}
+	if (!operationStarted) {
+		setDisplayValue("0");
+		operation.reset();
+		operationStarted = true;
+	}
+	let numberStr = getDisplayValue();
+	if (numberStr === "0") numberStr = "";
+	numberStr += num;
+	if (operation.operator === "") {
+		//operator is only empty while entering the firstNumber, this works, but replace with new boolean later
+		operation.num1 = parseFloat(numberStr);
+		operationStarted = true;
+	} else {
+		operation.num2 = parseFloat(numberStr);
+		operationReady = true;
+	}
+	return numberStr;
+}
+
 function formatResultForDisplay(number) {
-    if (number % 1 != 0) {
-        let resultStr = number.toString();
-        if (resultStr.length > 10) {
-            resultStr = resultStr.slice(0, 10);
-        }
-        return parseFloat(resultStr);
-    } else {
-        return number;
-    }
-}
-
-const operators = {
-    "÷":
-    {
-        operate: function (num1, num2) { return num1 / num2; }
-    },
-    "x":
-    {
-        operate: function (num1, num2) { return num1 * num2; }
-    },
-    "-":
-    {
-        operate: function (num1, num2) { return num1 - num2; }
-    },
-    "+":
-    {
-        operate: function (num1, num2) { return num1 + num2; }
-    }
+	if (number % 1 != 0) {
+		let resultStr = number.toString();
+		if (resultStr.length > 10) {
+			resultStr = resultStr.slice(0, 10);
+		}
+		return parseFloat(resultStr);
+	} else {
+		return number;
+	}
 }
 
 function setDisplayValue(newValue) {
-    document.getElementById("display").textContent = newValue;
+	document.getElementById("display").textContent = newValue;
 }
 
 function getDisplayValue() {
-    return document.getElementById("display").textContent;
+	return document.getElementById("display").textContent;
 }
 
 function deselectOperators() {
-    operatorSelected = false;
-    for (let button of opButtons) {
-        if (button.classList.contains("selected")) {
-            button.classList.remove("selected");
-        }
-    }
+	operatorSelected = false;
+	for (let button of opButtons) {
+		if (button.classList.contains("selected")) {
+			button.classList.remove("selected");
+		}
+	}
 }
 
 function clearAll() {
-    setDisplayValue("");
-    firstNumber = 0;
-    secondNumber = 0;
-    deselectOperators();
-    operator = "";
+	setDisplayValue("0");
+	operation.num1 = 0;
+	operation.num2 = 0;
+	deselectOperators();
+	operation.operator = "";
 }
 
 function changeMemory(button) {
-    const valueOnDisplay = getDisplayValue();
-    if (valueOnDisplay != "0" || valueOnDisplay != "") {
-        switch (button) {
-            case "M+":
-                memoryNumber += parseFloat(valueOnDisplay);
-                break;
-            case "M-":
-                memoryNumber -= parseFloat(valueOnDisplay);
-                break;
-        }
-    }
+	const valueOnDisplay = getDisplayValue();
+	if (valueOnDisplay != "0") {
+		switch (button) {
+			case "M+":
+				memoryNumber += parseFloat(valueOnDisplay);
+				break;
+			case "M-":
+				memoryNumber -= parseFloat(valueOnDisplay);
+				break;
+		}
+	}
 }
 
 function recallMemory() {
-    if (firstNumber === 0) {
-        firstNumber = memoryNumber
-        setDisplayValue(firstNumber);
-    }
-    if (operatorSelected) {
-        secondNumber = memoryNumber;
-        setDisplayValue(secondNumber);
-        deselectOperators();
-        operationReady = true;
-    }
+	if (operation.num1 === 0) {
+		operation.num1 = memoryNumber;
+		setDisplayValue(operation.num1);
+	}
+	if (operatorSelected) {
+		operation.num2 = memoryNumber;
+		setDisplayValue(operation.num2);
+		deselectOperators();
+		operationReady = true;
+	}
 }
 
 function clearMemory() {
-    memoryNumber = 0;
+	memoryNumber = 0;
 }
